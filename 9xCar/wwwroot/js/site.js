80fd93f4ac18e0512f02ff621cbc906b36466cbf
doardo9x <eduardo010304@gmail.com>
2024-04-01 10:58:25 -0300

    100% funcionando.

--- 9xCar/wwwroot/js/site.js
+++ 9xCar/wwwroot/js/site.js
@@ -4,9 +4,9 @@
     cards = document.getElementsByClassName("card");
     buttons = document.getElementsByClassName("btn-filter");
     for (i = 0; i < cards.length; i++) {
-        cards[i].parentElement.style.display = 'none';
+        cards[i].style.display = 'none';
         if (cards[i].classList.contains(type) || type === "all") {
-            cards[i].parentElement.style.display = 'block';
+            cards[i].style.display = 'block';
             count += 1;
         };
     };
@@ -25,7 +25,54 @@
         document.getElementById("btn-all").classList.add("btn-md");
     };
     if (count == 0)
-        document.getElementById("zeroPokemon").classList.remove("d-none");
+        document.getElementById("zeroCarro").classList.remove("d-none");
     else
-        document.getElementById("zeroPokemon").classList.add("d-none");
+        document.getElementById("zeroCarro").classList.add("d-none");
 }
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
